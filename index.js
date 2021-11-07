ad516c0aaa1728d75a8c182b3d141206b73a8a6e
Yeon <jyn990@gmail.com>
2021-11-07 19:46:06 +0900

    처음 저장소에 올림

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+const mongoose = require('mongoose')
+mongoose.connect('mongodb+srv://yeon:yeon00@boilerplate.hx0ya.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {
+	// useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false
+}).then(() => console.log('MongoDB Connected...'))
+  .catch(err => console.log(err))
+
+
+
+app.get('/', (req, res) => {
+  res.send('Hello World! 안녕하세요!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
